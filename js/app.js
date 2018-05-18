3f96baa06dda5d0c33b72a3097695e3f249338c0
Philippine du Merac <philippine.merac@gmail.com>
2018-05-17 19:29:36 -0700

    Welcome Page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-document.getElementsByTagName("li")[0].addEventListener("click",function(){
-	if(!this.parentNode.getAttribute("class")){
-		this.parentNode.setAttribute("class","slideDown");
-	}
-	else{
-		this.parentNode.removeAttribute("class");
-	}
+$(document).ready(function(){
+    $('.bwlist').hide();
+    $('.bw').click(function(){
+        $('.bwlist').css("color", "pink").slideToggle();
+        $('header').css('background-image',"url('images/GagosianLA.jpg')");
+    });
+
+    $('.discover').hide();
+    $('.artists').click(function(){
+        $('.discover').css("color", "pink").slideToggle();
+        $('header').css('background-image',"url('images/PlatoInLA.jpg')");
+    });
+
+    $('.blogging').hide();
+    $('.blog').click(function(){
+        $('.blogging').css("color", "pink").slideToggle();
+        $('header').css('background-image',"url('images/Ana-Serrano_Mustard-House.jpg')");
+    });
+
+    $('.belist').hide();
+    $('.be').click(function(){
+        $('.belist').css("color", "pink").slideToggle();
+        $('header').css('background-image',"url('images/Clay.jpg')");
+    });
 });
